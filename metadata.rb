049052474ad4257ada215f30c95245a80ce5af8f
byplayer <byplayer100@gmail.com>
2014-05-18 23:26:39 +0800

    add snappy recipe

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ description      'Installs/Configures cookbook-hadoop_single'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-%w(java).each do |cookbook|
+%w(java snappy).each do |cookbook|
   depends cookbook
 end
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,10 +6,9 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-%w( java ).each do |cookbook|
+%w(java snappy).each do |cookbook|
   include_recipe cookbook
 end
 
-include_recipe "hadoop_single::_hadoop_user"
-include_recipe "hadoop_single::_hadoop_install"
-
+include_recipe 'hadoop_single::_hadoop_user'
+include_recipe 'hadoop_single::_hadoop_install'
